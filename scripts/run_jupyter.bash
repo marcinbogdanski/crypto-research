493423b2fbc96d687842ca26b005e35ae471dd73
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2021-02-27 09:53:26 +0000

    Add Scrap_03_CheckEBuyer.ipynb

--- scripts/run_jupyter.bash
+++ scripts/run_jupyter.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "You must specify path to data folder!"
+    exit 1
+fi
+
+DATAPATH=$(realpath $1)
+
 docker run -it --rm \
   -p 9997:9997 \
-  -v /home/marcin/crypto-research:/app \
+  -v $PWD:/app \
+  -v $DATAPATH:/mnt/data:ro \
+  -w / \
   --entrypoint=jupyter \
   --env-file=dotenv-scan-3060ti.env \
   crypto-research lab \
