bfd092c95a6c9d943f6ed75e5b1b2a33af7ddcda
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2021-05-03 08:53:53 +0100

    VPN: robustify startup.bash

--- scripts/startup.bash
+++ scripts/startup.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-source scripts/add_dns_route.bash
-source scripts/ufw_up.bash
-source scripts/vpn_up.bash
-
-sleep 5
+if [ $VPN_ENABLE == "true" ]; then
+  echo "Adding DNS route, enabling UFW nad VPN"
+  source scripts/add_dns_route.bash
+  source scripts/ufw_up.bash
+  source scripts/vpn_up.bash
+elif [ $VPN_ENABLE == "false" ]; then
+  echo "Leavnig DNS untouched, omitting UFW and VPN"
+else
+  echo "Variable VPN_ENABLE must be set to either 'true' or 'false'. Exiting"
+  exit 1
+fi
 
 while true; do
   date
